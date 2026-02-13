1d973152fb235652d368fc2d665be890a765921d
santyxhavez <santyxhavez37@gmail.com>
2026-02-12 17:25:41 -0800

    hicimos una practica la cual utilizamos el for while if para pooder hacerla  README.md

--- sesion_03.py
+++ sesion_03.py
@@ -44,4 +44,13 @@ else:
 # jueves 
 # viernes
 
+mi_lista_2 = ["lunes","martes","miercoles","jueves","viernes"]
 
+contador = 0
+
+while contador < 3:
+    for i in mi_lista_2:
+        if i != "lunes":
+            print(i)
+    contador += 1
+    
