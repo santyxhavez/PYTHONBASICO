c768db1b071ef19cc9aee3b464a72d801340cb17
santyxhavez <santyxhavez37@gmail.com>
2026-03-02 18:10:05 -0800

    Nuevo Cambio

--- OOP/Enemigo.py
+++ OOP/Enemigo.py
@@ -1,25 +1,25 @@
 class Enemigo:
-    tipo_Enemigo: str
+    tipo_enemigo: str
     puntos_energia: int = 10
     ataque = 1
 
-    def __init__(self, tipo_Enemigo, puntos_energia=10, ataque=1):
-        self._tipo_Enemigo = tipo_Enemigo
+    def __init__(self, tipo_enemigo, puntos_energia=10, ataque=1):
+        self._tipo_enemigo = tipo_enemigo
         self.puntos_energia = puntos_energia
         self.ataque = ataque
 
-        def get_tipo_Enemigo(self):
-            return self.__tipo_Enemigo
+    def get_tipo_enemigo(self):
+        return self._tipo_enemigo
 
-        def habla(self):
-            print(f"Yo soy {self.__tipo_Enemigo}.preparando para pelear!!")
+    def habla(self):
+        print(f"Yo soy {self._tipo_enemigo}.preparando para pelear!!")
 
-        def camina(self):
-            print(f"{self.__tipo_Enemigo} se mueve cerca de ti!!! ")
+    def camina(self):
+        print(f"{self._tipo_enemigo} se mueve cerca de ti!!! ")
 
-        def atacar(self):
-            print(f"{self.__tipo_Enemigo} ataca con un {self.ataque}de dar") 
+    def atacar(self):
+        print(f"{self._tipo_enemigo} ataca con un {self.ataque}de dar") 
             
-        def ataque_especial(self):
-            print("Enemigo no tiene ataque especial")
+    def ataque_especial(self):
+        print("Enemigo no tiene ataque especial")
     
--- OOP/Zombie.py
+++ OOP/Zombie.py
@@ -5,14 +5,14 @@ class Zombie(Enemigo):
     def __init__(self, puntos_energia=10, ataque=1):
         super().__init__(tipo_enemigo="zombie", puntos_energia=puntos_energia, ataque=ataque)
 
-        def habla(self):
+    def habla(self):
             print("hummmmmm...")
 
-        def propagar_enfermedad(self):
+    def propagar_enfermedad(self):
             print("El zombie esta tratando de propagar la enfermedad")
         
-        def ataque_especial(self):
-            print("Zombie ataque especial")
+    def ataque_especial(self):
+        print("Zombie ataque especial")
         funciona_ataque_especial= random.random() <0.50
         if funciona_ataque_especial:
             self.puntos_energia += 2
